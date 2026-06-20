373533bdfa5c0437e94e0b1d98950056e219f708
unknown <adamnugraha339@gmail.com>
2026-06-20 12:56:07 +0700

    chore: revert back to Vercel Serverless configuration

--- src/index.js
+++ src/index.js
@@ -33,6 +33,11 @@ app.get("/", (req, res) => {
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+// Export untuk Vercel Serverless
+module.exports = app;
