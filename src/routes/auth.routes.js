8a9882fc5fde48c9e4702b8afb2d850aa715e3ae
muhammadarifbillah <arifbillahv19@gmail.com>
2026-06-20 17:42:32 +0700

    security: remove temp database url route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,8 +5,5 @@ const authController = require('../controllers/auth.controller');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/google-login', authController.googleLogin);
-router.get('/temp-db-url', (req, res) => {
-  res.json({ databaseUrl: process.env.DATABASE_URL, directUrl: process.env.DIRECT_URL });
-});
 
 module.exports = router;
